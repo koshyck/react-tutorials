25144003901c03cfd9be04efc53df83870292816
Ben <benkoshy@hotmail.com>
2020-08-02 23:19:39 +1000

    Api Call to weather.org working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import "./App.css";
 import WeatherCard from "./components/WeatherCard/component";
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=Sydney&country=Aus&units=metric&APPID=146b14eae13cbb3841ed3479114fd951"
+    );
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+  data().then((res) => console.log(res.main.temp));
   return (
     <div className="App">
       <div>
