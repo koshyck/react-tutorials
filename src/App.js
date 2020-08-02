84540f1303c7c4cd47851be899ac435a687b1810
Ben <benkoshy@hotmail.com>
2020-08-03 07:56:53 +1000

    episode 8 completed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import WeatherCard from "./components/WeatherCard/component";
 
 function App() {
-  const data = async () => {
+  const [query, setQuery] = useState("Sydney, au");
+  const [weather, setWeather] = useState({
+    temp: null,
+    city: null,
+    condition: null,
+    country: null,
+  });
+
+  const data = async (q) => {
     const apiRes = await fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=Sydney&country=Aus&units=metric&APPID=146b14eae13cbb3841ed3479114fd951"
+      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=146b14eae13cbb3841ed3479114fd951`
     );
     const resJSON = await apiRes.json();
     return resJSON;
   };
-  data().then((res) => console.log(res.main.temp));
+  //  data().then((res) => console.log(res.main.temp));
+  const handleSearch = (e) => {
+    e.preventDefault();
+    data(query).then((res) => {
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.sys.country,
+      });
+    });
+  };
+
+  useEffect(() => {
+    data(query).then((res) => {
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.country,
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
       <div>
-        <WeatherCard temp={-20} condition="Cloudy" city="London" country="GB" />
-      </div>
-      <div>
         <WeatherCard
-          temp={10}
-          condition="Haze"
-          city="Sydney"
-          country="Australia"
+          temp={weather.temp}
+          condition={weather.condition}
+          city={weather.city}
+          country={weather.country}
         />
       </div>
-      <div>
-        <WeatherCard
-          temp={40}
-          condition="Sunny"
-          city="Melbourne"
-          country="Australia"
-        />
+
+      <div align="center">
+        <form>
+          <input value={query} onChange={(e) => setQuery(e.target.value)} />
+          <button onClick={(e) => handleSearch(e)}>Set City</button>
+        </form>
       </div>
     </div>
   );
--- src/components/WeatherCard/Icon.js
+++ src/components/WeatherCard/Icon.js
@@ -1,18 +1,18 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const Icon = (props) => {
+const Icon = ({ condition }) => {
   const Icon = styled.img``;
 
   var icon = "";
-  switch (props.condition) {
+  switch (condition) {
     case "Cloudy":
       icon = `https://image.flaticon.com/icons/svg/149/149209.svg`;
       break;
     case "Clear":
       icon = `https://image.flaticon.com/icons/svg/2917/2917242.svg`;
       break;
-    case "Haze":
+    case "Clouds":
       icon = `https://image.flaticon.com/icons/svg/119/119075.svg`;
       break;
     default:
